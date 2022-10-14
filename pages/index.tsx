2e2cb3c7d20428c2d08598863f0fa925e5e63fbe
northfacegawd <chuck5732590@gmail.com>
2022-10-14 19:51:35 +0900

    fix: 문구 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = function () {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Cody On!</a>
         </h1>
 
         <p className={styles.description}>
