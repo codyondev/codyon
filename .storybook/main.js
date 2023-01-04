418cfda1563c4708680a5aee198053d7c2c6b48c
northfacegawd <chuck5732590@gmail.com>
2023-01-04 22:24:41 +0900

    chore: storybook 적용

---
+++ .storybook/main.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-addon-next-router',
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      path.resolve(__dirname, '..'),
+      'node_modules',
+      'styles',
+    ];
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@pages': path.resolve(__dirname, '../pages'),
+      '@components': path.resolve(__dirname, '../components'),
+      '@public': path.resolve(__dirname, '../assets'),
+      '@lib': path.resolve(__dirname, '../lib'),
+      '@hooks': path.resolve(__dirname, '../hooks'),
+      '@models': path.resolve(__dirname, '../models'),
+      '@styles': path.resolve(__dirname, '../styles'),
+      '@constant': path.resolve(__dirname, '../constant'),
+      '@mocks': path.resolve(__dirname, '../mocks'),
+      '@store': path.resolve(__dirname, '../store'),
+      '@requests': path.resolve(__dirname, '../requests'),
+    };
+
+    return config;
+  },
+};
--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,1 @@
-import { server } from '@mocks/server';
-import '@testing-library/jest-dom/extend-expect';
-
-beforeAll(() => server.listen());
-
-afterEach(() => server.resetHandlers());
-
-afterAll(() => server.close());
+import '@testing-library/jest-dom';
