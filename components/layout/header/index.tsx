41852f41c279a7004cf5d88394f9c2c2019b2289
northfacegawd <chuck5732590@gmail.com>
2023-02-05 13:26:55 +0900

    feat: 헤더 추가

--- components/layout/header/index.tsx
+++ components/layout/header/index.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import Logo from '@public/images/codyon_logo.svg';
 
 function Header() {
   return (
-    <header className="p-[15px] bg-white">
-      <Image src={Logo} alt="Logo Icon" width={18} height={18} />
+    <header className="h-[46px] bg-white border-b border-[#CECECE] flex items-center px-[15px]">
+      <Link href="/" className="flex">
+        <Image src={Logo} alt="Logo Icon" width={18} height={18} />
+        <span className="text-[13px] font-bold ml-[3px]">codi on</span>
+      </Link>
     </header>
   );
 }
