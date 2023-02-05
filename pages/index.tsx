afa180dfee33a4743693b2aaaba38bfc8099cd51
northfacegawd <chuck5732590@gmail.com>
2023-02-05 20:06:22 +0900

    feat: 롤모델 섹션 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,6 +70,52 @@ const Home: NextPage = function () {
           </div>
         </article>
       </section>
+      <section className="p-layoutX mt-[15px]">
+        <h2 className="font-bold text-lg">롤 모델 </h2>
+        <ul className="flex gap-x-[9px] mt-6">
+          <li className="w-[50%]">
+            <img
+              src="/images/ahepf.svg"
+              alt="BiBi"
+              className="w-full object-cover"
+            />
+            <div className="px-[8px]">
+              <span className="text-xs font-semibold">모델 ahepf</span>
+              <p
+                className="text-[11px] border-l-2 border-black pl-[9px] text-ellipsis overflow-hidden text-gray-75"
+                style={{
+                  display: '-webkit-box',
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: 'vertical',
+                }}
+              >
+                에스파 같은 스타일리의 원조!! 이 언니가 하면 모노톤이 화려함
+              </p>
+            </div>
+          </li>
+          <li className="w-[50%]">
+            <img
+              src="/images/bibi.svg"
+              alt="BiBi"
+              className="w-full object-cover"
+            />
+            <div className="px-[8px]">
+              <span className="text-xs font-semibold">가수 BIBI</span>
+              <p
+                className="text-[11px] border-l-2 border-black pl-[9px] text-ellipsis overflow-hidden text-gray-75"
+                style={{
+                  display: '-webkit-box',
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: 'vertical',
+                }}
+              >
+                꺄 비비 스타일링이 항상 예뻐요 당당한 성격까지 너무 닮고 싶어요
+                작곡까지 각볏한 비비...
+              </p>
+            </div>
+          </li>
+        </ul>
+      </section>
     </Layout>
   );
 };
