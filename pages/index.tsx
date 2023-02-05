6ef2c15781a77cfc36128b2b50256ba3cedac329
northfacegawd <chuck5732590@gmail.com>
2023-02-05 15:41:57 +0900

    feat: 카드 작업 중지

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -25,6 +26,48 @@ const Home: NextPage = function () {
             </strong>
             무신사에서 연말룩 첫 구매시 50% 할인
           </p>
+        </article>
+      </section>
+      <section className="p-layoutX mt-[15px]">
+        <h2 className="font-bold text-lg">패션 프로필</h2>
+        <article className="mt-6 rounded-md bg-[#242424] h-[150px] flex justify-center py-[10px]">
+          <div>
+            <div className="w-[85px] h-[105px] overflow-hidden rounded-[100%] outline-dashed outline-1 outline-offset-2 outline-white">
+              <img src="/images/example-1.svg" alt="" />
+            </div>
+          </div>
+          <div>
+            <p className="w-[125px] text-[#CECECE] text-[10px] font-light leading-[13px] break-all">
+              날이 좋아서, 날이 좋지 않아서, 날이 적당해서. 모든 날에 화려하고
+              싶었다.
+            </p>
+            <div className="flex gap-x-[10px]">
+              <button
+                className="text-darkmint text-[10px] border border-darkmint bg-[#404040] py-[2px] px-[8px] rounded-[10px]"
+                type="button"
+              >
+                Fancy
+              </button>
+              <button
+                className="text-darkmint text-[10px] border border-darkmint bg-[#404040] py-[2px] px-[8px] rounded-[10px]"
+                type="button"
+              >
+                drEssy
+              </button>
+              <button
+                className="text-darkmint text-[10px] border border-darkmint bg-[#404040] py-[2px] px-[8px] rounded-[10px]"
+                type="button"
+              >
+                Mature
+              </button>
+              <button
+                className="text-darkmint text-[10px] border border-darkmint bg-[#404040] py-[2px] px-[8px] rounded-[10px]"
+                type="button"
+              >
+                Gonzo
+              </button>
+            </div>
+          </div>
         </article>
       </section>
     </Layout>
