8d93dbeb366d97a50ed95bcd1251aa08315fa53f
northfacegawd <chuck5732590@gmail.com>
2023-02-11 21:02:57 +0900

    feat: 공유 이미지 변경

--- pages/semg/index.tsx
+++ pages/semg/index.tsx
@@ -12,66 +12,20 @@ const Semg: NextPage = () => {
       container: '#kakaotalk-sharing-btn',
       objectType: 'feed',
       content: {
-        title: '오늘의 디저트',
-        description: '아메리카노, 빵, 케익',
-        imageUrl:
-          'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        title: '패션 MBTI 테스트',
+        description: '당신의 패션 MBTI 유형을 확인해보세요',
+        imageUrl: 'https://www.codion.site/images/main.png',
         link: {
-          mobileWebUrl: 'https://developers.kakao.com',
-          webUrl: 'https://developers.kakao.com',
+          mobileWebUrl: 'https://www.codion.site/semg',
+          webUrl: 'https://www.codion.site/semg',
         },
-      },
-      itemContent: {
-        profileText: 'Kakao',
-        profileImageUrl:
-          'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-        titleImageUrl:
-          'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
-        titleImageText: 'Cheese cake',
-        titleImageCategory: 'Cake',
-        items: [
-          {
-            item: 'Cake1',
-            itemOp: '1000원',
-          },
-          {
-            item: 'Cake2',
-            itemOp: '2000원',
-          },
-          {
-            item: 'Cake3',
-            itemOp: '3000원',
-          },
-          {
-            item: 'Cake4',
-            itemOp: '4000원',
-          },
-          {
-            item: 'Cake5',
-            itemOp: '5000원',
-          },
-        ],
-        sum: 'Total',
-        sumOp: '15000원',
-      },
-      social: {
-        likeCount: 10,
-        commentCount: 20,
-        sharedCount: 30,
       },
       buttons: [
         {
           title: '웹으로 이동',
           link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            webUrl: 'https://developers.kakao.com',
-          },
-        },
-        {
-          title: '앱으로 이동',
-          link: {
-            mobileWebUrl: 'https://developers.kakao.com',
-            webUrl: 'https://developers.kakao.com',
+            mobileWebUrl: 'https://www.codion.site/semg',
+            webUrl: 'https://www.codion.site/semg',
           },
         },
       ],
@@ -89,8 +43,9 @@ const Semg: NextPage = () => {
         현재 총 20,230,114명이 참여했어요.
       </p>
       <div className="mt-6 flex justify-center gap-12">
-        <button id="kakaotalk-sharing-btn">KakaoShare</button>
-
+        <button type="button" id="kakaotalk-sharing-btn">
+          <img src="/images/kakao.svg" alt="카카오 아이콘" />
+        </button>
         <button type="button">
           <img src="/images/chain.svg" alt="공유 아이콘" />
         </button>
