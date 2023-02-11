6e237af95751e21ef04e2ebfff1452a34ddcb5ae
northfacegawd <chuck5732590@gmail.com>
2023-02-11 20:37:51 +0900

    feat: 카카오톡 공유 기능 추가 및 타입 설정

--- pages/semg/index.tsx
+++ pages/semg/index.tsx
@@ -1,10 +1,83 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Layout from '@components/layout';
+import { DefaultFeedSettings } from '@custom-types/kakao';
 
 const Semg: NextPage = () => {
+  useEffect(() => {
+    if (!window.Kakao.isInitialized()) return;
+    window.Kakao.Share.createDefaultButton<DefaultFeedSettings>({
+      container: '#kakaotalk-sharing-btn',
+      objectType: 'feed',
+      content: {
+        title: '오늘의 디저트',
+        description: '아메리카노, 빵, 케익',
+        imageUrl:
+          'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com',
+        },
+      },
+      itemContent: {
+        profileText: 'Kakao',
+        profileImageUrl:
+          'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        titleImageUrl:
+          'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        titleImageText: 'Cheese cake',
+        titleImageCategory: 'Cake',
+        items: [
+          {
+            item: 'Cake1',
+            itemOp: '1000원',
+          },
+          {
+            item: 'Cake2',
+            itemOp: '2000원',
+          },
+          {
+            item: 'Cake3',
+            itemOp: '3000원',
+          },
+          {
+            item: 'Cake4',
+            itemOp: '4000원',
+          },
+          {
+            item: 'Cake5',
+            itemOp: '5000원',
+          },
+        ],
+        sum: 'Total',
+        sumOp: '15000원',
+      },
+      social: {
+        likeCount: 10,
+        commentCount: 20,
+        sharedCount: 30,
+      },
+      buttons: [
+        {
+          title: '웹으로 이동',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+        {
+          title: '앱으로 이동',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+      ],
+    });
+  }, []);
+
   return (
     <Layout showGNB={false}>
       <img
@@ -16,9 +89,8 @@ const Semg: NextPage = () => {
         현재 총 20,230,114명이 참여했어요.
       </p>
       <div className="mt-6 flex justify-center gap-12">
-        <button type="button">
-          <img src="/images/kakao.svg" alt="카카오 아이콘" />
-        </button>
+        <button id="kakaotalk-sharing-btn">KakaoShare</button>
+
         <button type="button">
           <img src="/images/chain.svg" alt="공유 아이콘" />
         </button>
